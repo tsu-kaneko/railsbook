9907cbee96876fbb5dec85e66c332c7900379b46
金子 翼 <gamuuma@gmail.com>
2016-12-25 19:25:29 +0900

    書籍の一覧表示の処理追加

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,9 +1,13 @@
 class HelloController < ApplicationController
-	def index
+	def  index
 		render text: 'こんにちは、世界'
 	end
 	
-	def view
+	def  view
 		@msg = 'こんにちは、世界！'
+	end
+	
+	def  list
+		@books = Book.all
 	end
 end
